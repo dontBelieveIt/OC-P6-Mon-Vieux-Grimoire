dc8b93ab636535f03b10fca26ab4b964e8ee920a
Roxane <roxane.vidoni@gmail.com>
2024-07-20 12:51:50 +0200

    Added moongose package. Connexion to MongoDB = successful.

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,23 @@
+const mongoose = require('mongoose');
 const express = require('express');
+
 
 const app = express();
 
+mongoose.connect('mongodb+srv://visitor:ickI98YeFKJbwzh7@cluster0.s8f2adz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', 
+    { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true 
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MangoDB échouee :( !'));
+    
+//To avoid CORS
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
 module.exports = app;
