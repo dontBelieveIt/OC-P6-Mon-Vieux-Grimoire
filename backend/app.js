0969945b249639f1c9f011f6a85d48e8fb129a12
Roxane <roxane.vidoni@gmail.com>
2024-07-20 13:50:21 +0200

    Backend, auth : install of mongoose-unique-validator + bcrypt for user authentification

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const express = require('express');
 
-
 const app = express();
 
+// Connexion to MongoDB, Cluster 0
 mongoose.connect('mongodb+srv://visitor:ickI98YeFKJbwzh7@cluster0.s8f2adz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', 
     { 
         useNewUrlParser: true,
@@ -18,6 +18,13 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
+  });
+
+  app.post('/api/stuff', (req, res, next) => {
+    console.log('This is my first post request :) ');
+    res.status(201).json({
+      message: 'Objet créé !'
+    });
   });
 
 module.exports = app;
