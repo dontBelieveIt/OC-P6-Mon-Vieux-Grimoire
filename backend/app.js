58514000f063363dddc684854fef85932314dc28
Roxane <roxane.vidoni@gmail.com>
2024-07-22 15:08:39 +0200

    addition of package body-parser. Signup route = 200 OK. Creation of user account is a success.

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,13 @@
-const mongoose = require('mongoose');
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const bodyParser = require('body-parser');
+require('body-parser-xml')(bodyParser);
 const cors = require('cors'); 
 
 const app = express();
+app.use(express.json());
+app.use(bodyParser.xml());
 
 const userRoutes = require('./routes/user');
 const bookRoutes = require('./routes/book');
@@ -19,12 +24,12 @@ mongoose.connect('mongodb+srv://visitor:ickI98YeFKJbwzh7@cluster0.s8f2adz.mongod
 
 //To avoid CORS
 app.use(cors());
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-//     next();
-//   });
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
 app.use('/api/auth', userRoutes); 
 app.use('/api/books', bookRoutes);
