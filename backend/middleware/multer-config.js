6735df68435b0b8ce8d75b0a29c4552a85a45286
Roxane <roxane.vidoni@gmail.com>
2024-07-26 11:30:03 +0200

    function to optimize image implemented. Working as intended.

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,10 +1,17 @@
 const multer = require('multer');
+const sharp = require('sharp'); 
+const path = require('path');
 
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
   'image/png': 'png'
 };
+
+function fileFilter(req, file, cb) {
+  cb(MIME_TYPES, true)
+  cb(new Error("Le fichier doit-être une image ! (jpg, jpeg, png)."))
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
@@ -13,8 +20,27 @@ const storage = multer.diskStorage({
   filename: (req, file, callback) => {
     const name = file.originalname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
+    callback(null, name + Date.now() + '.' + Math.round(Math.random() *1E9) + extension);
   }
 });
 
 module.exports = multer({storage: storage}).single('image');
+
+module.exports.imgOptimization = (req, res, next) => {
+  if (req.file) { // check if request has a downloaded file
+    const filePath = req.file.path;
+    const output = path.join('images', `optimized_${req.file.filename}`); // where picture will be sent, and name
+    sharp.cache(false);
+    sharp(filePath)         
+        .resize({width: 412, height: 520,  fit: 'cover' }) // Resize picture
+        .webp({ quality: 85 })
+        .toFile(output) // Upload new picture 
+        .then(() => {
+            req.file.path = output;
+            console.log("Optimisation effectuée !")
+            next();
+            })
+        .catch(err => next(err))
+} else {
+    return next();
+}};
--- backend/routes/book.router.js
+++ backend/routes/book.router.js
@@ -7,12 +7,11 @@ const multer = require('../middleware/multer-config');
 const bookCtrl = require('../controllers/books.controller');
 
 router.get('/', bookCtrl.getBooks); 
-router.post('/', auth, multer, bookCtrl.createBooks); 
+router.post('/', auth, multer, multer.imgOptimization, bookCtrl.createBooks); 
 
 router.get('/bestrating', bookCtrl.bestRatingBooks);
-
 router.get('/:id', bookCtrl.getThisBook); 
-router.put('/:id', auth, multer, bookCtrl.editThisBook); 
+router.put('/:id', auth, multer, multer.imgOptimization, bookCtrl.editThisBook); 
 router.delete('/:id', auth, bookCtrl.deleteThisBook); 
 
 router.post('/:id/rating', auth, bookCtrl.noteThisBook); 
